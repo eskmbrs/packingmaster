743ef021088b31686f0cee139fa1a0c2fd2d9a5f
onishi <onishi@hapis.k.u-tokyo.ac.jp>
2020-12-25 10:48:23 +0900

    first commit

---
+++ Assets/prefabTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class prefabTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GameObject obj = (GameObject)Resources.Load("MyObject");
+            // Cubeプレハブを元に、インスタンスを生成、
+            Instantiate(obj, new Vector3(-2f, 2.6f, 0.0f), Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GameObject obj = (GameObject)Resources.Load("MySphere");
+            // Cubeプレハブを元に、インスタンスを生成、
+            Instantiate(obj, new Vector3(-2f, 2.6f, 0.0f), Quaternion.identity);
+        }
+
+    }
+
+}
