c5ea8febe05bbc6c24f7ecae7cf91c80cd077cab
onishi <onishi@hapis.k.u-tokyo.ac.jp>
2021-01-23 07:28:28 +0900

    バックアップ

--- Assets/handController.cs
+++ Assets/handController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class handController : MonoBehaviour
 {
     private Rigidbody rBody;
-
+    
+    [SerializeField]
+    public bool controlled;
 
     // Start is called before the first frame update
     void Start()
@@ -24,18 +26,20 @@ public class handController : MonoBehaviour
         // 左に移動
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            this.transform.Translate(-0.01f, 0.0f, 0.0f);
+            this.transform.Translate(0.1f, 0.0f, 0.0f);
         }
         // 右に移動
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            this.transform.Translate(0.01f, 0.0f, 0.0f);
+            this.transform.Translate(-0.1f, 0.0f, 0.0f);
         }
       
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             rBody = this.GetComponent<Rigidbody>();
             rBody.useGravity = true;
+
+            controlled = false;
         }
   
 
