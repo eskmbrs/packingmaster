919b85004c1ac08e8c6e5a57f14aeefd07619753
onishi <onishi@hapis.k.u-tokyo.ac.jp>
2021-02-05 15:17:52 +0900

    test

---
+++ Assets/test.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    private Rigidbody rBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 前に移動
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.transform.Translate(-0.1f, 0.0f, 0.0f);
+        }
+        // 後ろに移動
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.transform.Translate(0.1f, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rBody = this.GetComponent<Rigidbody>();
+            rBody.useGravity = true;
+        }
+    }
+}
