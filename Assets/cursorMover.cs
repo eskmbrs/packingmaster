e43d189d588948f6316d27576373b6a1a73f0fb8
onishi <onishi@hapis.k.u-tokyo.ac.jp>
2021-02-07 15:11:02 +0900

    カーソルを指に MyAssetsを整理

---
+++ Assets/cursorMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cursorMover : MonoBehaviour
+{
+
+    public Texture2D handCursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(handCursor,Vector2.zero,CursorMode.Auto);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
