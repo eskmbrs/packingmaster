8aa72fb7abd18e0614ca599ff3e63c08b585f921
Rihito Iijima <iijima@onefinancial.jp>
2021-05-30 13:52:49 +0900

    add Game Clear Panel.cs

---
+++ Assets/MyAssets/Scripts/GameClearPanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameClearPanel : MonoBehaviour
+{
+    private GameController gameController;
+
+
+  void Awake() {
+        gameController = GameObject.Find("GameController").GetComponent<GameController>();
+  }
+
+    void Update() {
+    }
+
+    void OnEnable() {
+        StartCoroutine(EnableSequencialy());
+    }
+
+    void OnDisable() {
+    }
+
+    IEnumerator EnableSequencialy()
+    {
+        yield break;
+    }
+}
