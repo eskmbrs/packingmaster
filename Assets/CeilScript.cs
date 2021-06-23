244606747bfb68a2968d88a9c067d47f6010f914
Rihito Iijima <iijima@onefinancial.jp>
2021-06-23 22:47:50 +0900

    add new scripts

---
+++ Assets/CeilScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilScript : MonoBehaviour
+{
+    public bool IsColliding = false;
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Collide!!!!");
+    }
+
+    private void OnTriggerStay(Collider other) {
+        Debug.Log("Colliding!!!!");
+        IsColliding = true;
+    }
+
+    private void OnTriggerExit(Collider other) {
+        IsColliding = false;
+    }
+}
